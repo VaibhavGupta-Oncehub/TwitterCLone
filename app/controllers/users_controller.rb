5ca7fabc611087d0504fc67cd5e0baf84f8b1341
vaibhav gupta <vaibhav.gupta@oncehub.com>
2022-05-24 07:51:20 +0530

    completed the crud operations on users with devise

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user! 
+
+  def show
+    @user= User.find(params[:id])
+
+    if @user === nil
+      flash.now[:alert] = "You should be logged in to view your profile."
+      redirect_to root_path
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'home#home'
   devise_for :users
+  resources :users, :only => [:show]
+
 end
